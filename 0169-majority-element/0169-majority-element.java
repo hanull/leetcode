a196102fad10c90ced092f730d2ced50d99b7f6a
Hanul Lee <46413629+hanull@users.noreply.github.com>
2022-12-26 09:42:52 +0900

    Time: 1 ms (99.92%), Space: 45.7 MB (94.83%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    // Moore voting algorithm
+    public int majorityElement(int[] nums) {
+        int count = 1;
+        int major = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            if (nums[i] == major) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                major = nums[i];
+                count = 1;
+            }
+        }
+        return major;
+    }
+}
