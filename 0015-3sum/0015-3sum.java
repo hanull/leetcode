3bb87d23b5b4538d122d1a6fdcbb4823b19cd2c2
Hanul Lee <46413629+hanull@users.noreply.github.com>
2023-01-06 12:25:56 +0900

    Time: 27 ms (81.24%), Space: 46.2 MB (93.36%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,25 +4,22 @@ class Solution {
         
         List<List<Integer>> answer = new ArrayList<>();
         
-        if (nums == null || nums.length < 3) return answer;
-        
-        for (int i=0; i<nums.length-2; i++) {
-            if (i>0 && nums[i] == nums[i-1]) continue;
+        for (int i=0; i<nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
             int left = i+1;
-            int right = nums.length-1;
-            
+            int right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     answer.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left+1 < right && nums[left]==nums[left+1]) left++;
-                    while (right-1 > left && nums[right]==nums[right-1]) right--;
                     left++;
                     right--;
-                } else if (sum < 0) {
-                    left++;
+                    while(left < right && nums[left-1] == nums[left]) left++;
+                    while(right > left && nums[right] == nums[right + 1]) right--;
+                } else if (sum > 0) {
+                    right--;
                 } else {
-                    right--;
+                    left++;
                 }
             }
         }
