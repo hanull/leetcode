55c4b3b35109f0f8d6a8d8b06fa79c9f28aa33b7
Hanul Lee <46413629+hanull@users.noreply.github.com>
2023-01-11 10:44:15 +0900

    Time: 4 ms (56.72%), Space: 42.4 MB (90.24%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> answer = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfs(0, 0, new ArrayList<>(), candidates, target, answer);
+        return answer;
+    }
+    
+    public void dfs(int index, int sum, List<Integer> pickNums, int[] candidates, int target, List<List<Integer>> answer) {
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            answer.add(new ArrayList<>(pickNums));
+            return;
+        }
+        
+        for (int i=index; i<candidates.length; i++) {
+            if (i > 0 && candidates[i-1] == candidates[i]) continue;
+            pickNums.add(candidates[i]);
+            sum += candidates[i];
+            dfs(i, sum, pickNums, candidates, target, answer);
+            pickNums.remove(pickNums.size()-1);
+            sum -= candidates[i];
+        }
+    }
+}
