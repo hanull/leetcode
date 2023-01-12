06832a2fdcdac6d87912d9cac06f1d7440ce13b7
Hanul Lee <46413629+hanull@users.noreply.github.com>
2023-01-12 10:48:32 +0900

    Time: 1 ms (94.47%), Space: 42.8 MB (63.52%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,7 +1,14 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        backtracking(0, nums, new ArrayList<>(), answer);
+        
+        // backtracking(0, nums, new ArrayList<>(), answer); 백트래킹으로 풀이
+        
+        Arrays.sort(nums);
+        do {
+            answer.add(getNums(nums));
+        } while(isNextPermute(nums));
+        
         return answer;
     }
     
@@ -19,5 +26,35 @@ class Solution {
             tempNums.remove(tempNums.size() - 1);
             count--;
         }
+    }
+    
+    public boolean isNextPermute(int[] nums) {
+        int i=nums.length-1;
+        while (i>0 && nums[i-1] >= nums[i]) i--;
+        
+        if (i<=0) return false;
+        
+        int j=nums.length-1;
+        while (nums[j] <= nums[i-1]) j--;
+        
+        swap(i-1,j,nums);
+        
+        j=nums.length-1;
+        while(i<j) swap(i++,j--,nums);
+        return true;
+    }
+    
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public List<Integer> getNums(int[] nums) {
+        List<Integer> newOrderNums = new ArrayList<>();
+        for (int num:nums) {
+            newOrderNums.add(num);
+        }
+        return newOrderNums;
     }
 }
