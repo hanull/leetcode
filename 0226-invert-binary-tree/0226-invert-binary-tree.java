d263d31423231f7ee9ca4721404f744099a24688
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-01-30 17:40:21 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (68.12%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -14,34 +14,19 @@
  * }
  */
 class Solution {
-    // dfs
-    public TreeNode invertTree2(TreeNode root) {
-        if (root == null) return root;
-        
-        TreeNode left = root.left;
-        root.left = invertTree(root.right);
-        root.right = invertTree(left);
-        
-        return root;
-    }
-    
-    // bfs
     public TreeNode invertTree(TreeNode root) {
-        if (root == null) return root;
+        TreeNode answer = root;
         
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode currentNode = q.poll();
-            TreeNode left = currentNode.left;
-            
-            currentNode.left = currentNode.right;
-            currentNode.right = left;
-            
-            if (currentNode.left != null) q.add(currentNode.left);
-            if (currentNode.right != null) q.add(currentNode.right);
+        if (root == null) {
+            return root;
         }
         
-        return root; 
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        return answer;
     }
 }
