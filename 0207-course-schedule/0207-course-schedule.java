55ee37f0264f1311a370bd32649ce3fb1cb5cb11
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-04-18 16:53:55 +0900

    Time: 4 ms (81.03%), Space: 44.4 MB (75.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,45 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<Integer>[] courses = new List[numCourses];
-
-        for (int[] ints : prerequisites) {
-            int target = ints[0];
-            int prerequisite = ints[1];
-            
-            if (courses[prerequisite] == null) {
-                courses[prerequisite] = new ArrayList<>();
-            }
-            courses[prerequisite].add(target);
+        List<Integer>[] nodes = new List[numCourses];
+        for (int i=0; i<numCourses; i++) {
+            nodes[i] = new ArrayList<>();
         }
         
-        boolean[] visited = new boolean[numCourses];
-        boolean[] finished = new boolean[numCourses];
-        for (int i=0; i<numCourses; i++) {
-            if (hasCycle(i, visited, finished, courses)) {
-                return false;
-            }
+        int[] indegrees = new int[numCourses];
+        for (int[] ints : prerequisites) {
+            int to = ints[0];
+            int from = ints[1];
+            
+            indegrees[to]++;
+            nodes[from].add(to);
         }
-        return true;
+        
+        return isPossibleToSort(indegrees, nodes, numCourses);
     }
     
-    private boolean hasCycle(int num, boolean[] visited, boolean[] finished, List<Integer>[] courses) {
-        if (courses[num] == null) {
-            return false;
-        }
+    private boolean isPossibleToSort(int[] indegrees, List<Integer>[] nodes, int numCourses) {
+        Queue<Integer> q = new ArrayDeque<>();
         
-        visited[num] = true;
-        for (int next : courses[num]) {
-            if (!visited[next]) {
-                boolean flag = hasCycle(next, visited, finished, courses);
-                if (flag) {
-                    return true;
-                }
-            } else if (!finished[next]) {
-                return true;
+        for (int i=0; i<numCourses; i++) {
+            if (indegrees[i] == 0) {
+                q.add(i);
             }
         }
-        finished[num] = true;
         
-        return false;
+        Queue<Integer> answer = new ArrayDeque<>();
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            answer.add(node);
+            
+            for (int nextNode : nodes[node]) {
+                indegrees[nextNode]--;
+                
+                if (indegrees[nextNode] == 0) {
+                    q.add(nextNode);
+                }
+            }
+        }
+        
+        return answer.size() == numCourses;
     }
 }
