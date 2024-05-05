8f88fc8e687c954c579fc5b867776d7e973784b8
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-05-05 23:40:36 +0900

    Time: 0 ms (100.00%), Space: 40.1 MB (73.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i=3; i<=n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        int[] answer = new int[n + 1];
+        answer[0] = 1;
+        answer[1] = 1;
+        for (int i=2; i<=n; i++) {
+            answer[i] = answer[i - 1] + answer[i - 2];
         }
         
-        return dp[n];
+        return answer[n];
     }
 }
