df4ac8e1e218438c003725e79667c0c150e783a3
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-06-05 09:14:15 +0900

    Time: 3 ms (33.21%), Space: 44.7 MB (29.57%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,33 @@
-class Solution {
+class Solution { 
+    
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> answer = new ArrayList<>();
+        
         Arrays.sort(candidates);
-        dfs(0, 0, new ArrayList<>(), candidates, target, answer);
+        
+        if (candidates[0] > target) {
+            return answer;
+        }
+        
+        dfs(0, 0, candidates, target, new ArrayList<>(), answer);
+        
         return answer;
     }
     
-    public void dfs(int index, int sum, List<Integer> pickNums, int[] candidates, int target, List<List<Integer>> answer) {
+    private void dfs(int index, int sum, int[] candidates, int target, List<Integer> nums, List<List<Integer>> answer) {
         if (sum > target) {
             return;
         }
+        
         if (sum == target) {
-            answer.add(new ArrayList<>(pickNums));
+            answer.add(new ArrayList<>(nums));
             return;
         }
         
         for (int i=index; i<candidates.length; i++) {
-            pickNums.add(candidates[i]);
-            sum += candidates[i];
-            dfs(i, sum, pickNums, candidates, target, answer);
-            pickNums.remove(pickNums.size()-1);
-            sum -= candidates[i];
+            nums.add(candidates[i]);
+            dfs(i, sum + candidates[i], candidates, target, nums, answer);
+            nums.remove(nums.size() - 1);
         }
     }
 }
