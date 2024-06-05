9a95b52075e628d0d6585023b10b16eece059ec3
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-06-05 09:25:40 +0900

    Time: 1 ms (93.86%), Space: 44.2 MB (84.82%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,59 +2,28 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
         
-        // backtracking(0, nums, new ArrayList<>(), answer); 백트래킹으로 풀이
-        
         Arrays.sort(nums);
-        do {
-            answer.add(getNums(nums));
-        } while(isNextPermute(nums));
+        
+        dfs(0, new boolean[nums.length], nums, new ArrayList<>(), answer);
         
         return answer;
     }
     
-    public void backtracking(int count, int[] nums, List<Integer> tempNums, List<List<Integer>> answer) {
-        if (count == nums.length) {
-            answer.add(new ArrayList<>(tempNums));
+    private void dfs(int count, boolean[] visited, int[] candidates, List<Integer> nums, List<List<Integer>> answer) {
+        if (count == candidates.length) {
+            answer.add(new ArrayList<>(nums));
             return;
         }
         
-        for (int i=0; i<nums.length; i++) {
-            if (tempNums.contains(nums[i])) continue;
-            tempNums.add(nums[i]);
-            count++;
-            backtracking(count, nums, tempNums, answer);
-            tempNums.remove(tempNums.size() - 1);
-            count--;
+        for (int i=0; i<candidates.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            visited[i] = true;
+            nums.add(candidates[i]);
+            dfs(count + 1, visited, candidates, nums, answer);
+            nums.remove(nums.size() - 1);
+            visited[i] = false;
         }
-    }
-    
-    public boolean isNextPermute(int[] nums) {
-        int i=nums.length-1;
-        while (i>0 && nums[i-1] >= nums[i]) i--;
-        
-        if (i<=0) return false;
-        
-        int j=nums.length-1;
-        while (nums[j] <= nums[i-1]) j--;
-        
-        swap(i-1,j,nums);
-        
-        j=nums.length-1;
-        while(i<j) swap(i++,j--,nums);
-        return true;
-    }
-    
-    public void swap(int i, int j, int[] nums) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    public List<Integer> getNums(int[] nums) {
-        List<Integer> newOrderNums = new ArrayList<>();
-        for (int num:nums) {
-            newOrderNums.add(num);
-        }
-        return newOrderNums;
     }
 }
