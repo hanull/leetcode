aa846436585f10b0e5cdec240581b9ae2c5588e2
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-06-23 01:07:57 +0900

    Time: 1 ms (100.00%), Space: 42.4 MB (30.36%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,63 +1,27 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int lenA = a.length();
-        int lenB = b.length();
+        int lenA = a.length() - 1;
+        int lenB = b.length() - 1;
+        int carry = 0;
         StringBuilder answer = new StringBuilder();
-        int temp = 0;
-        while (lenA > 0 && lenB > 0) {
-            int total = (a.charAt(lenA - 1) - '0') + (b.charAt(lenB - 1) - '0') + temp;
-            
-            if (total >= 2) {
-                temp = 1;
-                total -= 2;
-            } else {
-                temp = 0;
+        
+        while (lenA >= 0 || lenB >= 0 || carry == 1) {
+            if (lenA >= 0) {
+                carry += a.charAt(lenA) - '0';
+            }
+            if (lenB >= 0) {
+                carry += b.charAt(lenB) - '0';
             }
             
-            answer.append(total);
+            int num = carry % 2;
+            answer.append(num);
+            
+            carry /= 2;
             
             lenA--;
             lenB--;
         }
         
-        if (lenA > 0) {
-            while (lenA > 0) {
-                int total = temp + (a.charAt(lenA - 1) - '0');
-                
-                if (total >= 2) {
-                    temp = 1;
-                    total -= 2;
-                } else {
-                    temp = 0;
-                }
-                
-                answer.append(total);
-                
-                lenA--;
-            }
-        } else {
-            while (lenB > 0) {
-                int total = temp + (b.charAt(lenB - 1) - '0');
-                
-                if (total >= 2) {
-                    temp = 1;
-                    total -= 2;
-                } else {
-                    temp = 0;
-                }
-                
-                answer.append(total);
-                
-                lenB--;
-            }
-        }
-        
-        if (temp == 1) {
-            answer.append(1);
-        }
-        
-        answer.reverse();
-        
-        return answer.toString();
+        return answer.reverse().toString();
     }
 }
