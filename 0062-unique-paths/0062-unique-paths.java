4f1ef19aee3fd31af8719bfa805fad3bb3c81156
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-08-07 00:08:51 +0900

    Time: 0 ms (100.00%), Space: 40 MB (92.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,29 +1,20 @@
 class Solution {
 
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i=1; i<=m; i++) {
-            for (int j=1; j<=n; j++) {
-                dp[i][j] = -1;
+        int[][] dp = new int[m][n];
+        for (int i=0; i<m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j=0; j<n; j++) {
+            dp[0][j] = 1;
+        }
+        
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
         
-        return dfs(1, 1, m, n, dp);
-    }
-    
-    private int dfs(int x, int y, int m, int n, int[][] dp) {
-        if (x > m || y > n) {
-            return 0;
-        }
-        
-        if (x == m && y == n) {
-            return 1;
-        }
-        
-        if (dp[x][y] != -1) {
-            return dp[x][y];
-        }
-        
-        return dp[x][y] = dfs(x + 1, y, m, n, dp) + dfs(x, y + 1, m, n, dp);
+        return dp[m-1][n-1];
     }
 }
