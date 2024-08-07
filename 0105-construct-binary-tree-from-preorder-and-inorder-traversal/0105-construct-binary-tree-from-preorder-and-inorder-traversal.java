3494668fa52d158337daa3ff344c97f44ef7bfa0
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-08-08 01:36:00 +0900

    Time: 2 ms (70.12%), Space: 44.3 MB (40.45%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,32 +15,31 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return makeTree(0, preorder.length - 1, preorder, 0, inorder.length - 1, inorder);
+        Map<Integer, Integer> inorderIndexes = toMapInorderIndexes(inorder);
+        return makeTree(0, preorder.length - 1, preorder, 0, inorder.length - 1, inorder, inorderIndexes);
     }
     
-    private TreeNode makeTree(int preorderStart, int preorderEnd, int[] preorder, int inorderStart, int inorderEnd, int[] inorder) {
+    private Map<Integer, Integer> toMapInorderIndexes(int[] inorder) {
+        Map<Integer, Integer> indexes = new HashMap<>();
+        for (int i=0; i<inorder.length; i++) {
+            indexes.put(inorder[i], i);
+        }
+        return indexes;
+    }
+    
+    private TreeNode makeTree(int preorderStart, int preorderEnd, int[] preorder, int inorderStart, int inorderEnd, int[] inorder, Map<Integer,Integer> inorderIndexes) {
         if (preorderStart > preorderEnd || inorderStart > inorderEnd) {
             return null;
         }
         
         int parentValue = preorder[preorderStart];
-        int inorderParentIndex = getInorderParentIndex(parentValue, inorderStart, inorderEnd, inorder);
+        int inorderParentIndex = inorderIndexes.get(parentValue);
         int countOfLeftSubNodes = inorderParentIndex - inorderStart;
         
         TreeNode root = new TreeNode(parentValue);
-        root.left = makeTree(preorderStart + 1, preorderStart + countOfLeftSubNodes, preorder, inorderStart, inorderParentIndex - 1, inorder);
-        root.right = makeTree(preorderStart + countOfLeftSubNodes + 1, preorderEnd, preorder, inorderParentIndex + 1, inorderEnd, inorder);
+        root.left = makeTree(preorderStart + 1, preorderStart + countOfLeftSubNodes, preorder, inorderStart, inorderParentIndex - 1, inorder, inorderIndexes);
+        root.right = makeTree(preorderStart + countOfLeftSubNodes + 1, preorderEnd, preorder, inorderParentIndex + 1, inorderEnd, inorder, inorderIndexes);
         
         return root;
-    }
-    
-    private int getInorderParentIndex(int target, int start, int end, int[] inorder) {
-        for (int i=start; i<=end; i++) {
-            if (inorder[i] == target) {
-                return i;
-            }
-        }
-        
-        return -1;
     }
 }
