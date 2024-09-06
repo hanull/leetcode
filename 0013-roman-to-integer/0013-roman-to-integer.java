4f1c50455aaed589469e6d346a73239313c08362
Hanul Lee <46413629+hanull@users.noreply.github.com>
2024-09-06 22:07:15 +0900

    Time: 5 ms (58.49%), Space: 44.8 MB (25.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -12,45 +12,11 @@ class Solution {
         char[] romanNumerals = s.toCharArray();
         int answer = 0;
         for (int i=0; i<romanNumerals.length; i++) {
-            char ch = romanNumerals[i];
-            
-            if (ch == 'I' && i<romanNumerals.length - 1 && romanNumerals[i + 1] == 'V') {
-                answer += 4;
-                i++;
-                continue;
+            if (i < romanNumerals.length - 1 && symbolToValue.get(romanNumerals[i]) < symbolToValue.get(romanNumerals[i + 1])) {
+                answer -= symbolToValue.get(romanNumerals[i]);
+            } else {
+                answer += symbolToValue.get(romanNumerals[i]);
             }
-            
-            if (ch == 'I' && i<romanNumerals.length - 1 && romanNumerals[i + 1] == 'X') {
-                answer += 9;
-                i++;
-                continue;
-            }
-            
-            if (ch == 'X' && i<romanNumerals.length - 1 && romanNumerals[i + 1] == 'L') {
-                answer += 40;
-                i++;
-                continue;
-            }
-            
-            if (ch == 'X' && i<romanNumerals.length - 1 && romanNumerals[i + 1] == 'C') {
-                answer += 90;
-                i++;
-                continue;
-            }
-            
-            if (ch == 'C' && i<romanNumerals.length - 1 && romanNumerals[i + 1] == 'D') {
-                answer += 400;
-                i++;
-                continue;
-            }
-            
-            if (ch == 'C' && i<romanNumerals.length - 1 && romanNumerals[i + 1] == 'M') {
-                answer += 900;
-                i++;
-                continue;
-            }
-            
-            answer += symbolToValue.get(ch);
         }
         
         return answer;
